467a448face45876e7e177e153ab11d91c98c334
Vikas Seervi <218r1a05c8@cmrec.ac.in>
2023-01-18 00:40:32 +0530

    welcome page

---
+++ app/src/main/java/com/example/docshub/WelcomePage.kt
@@ -0,0 +1,11 @@
+package com.example.docshub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_page)
+    }
+}
